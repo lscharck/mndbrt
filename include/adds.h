71018d496ba1540b680d3641e8e89915cc24edf1
lscharck <lscharck@gmail.com>
2021-12-22 19:45:57 -0600

    increase iters

--- include/adds.h
+++ include/adds.h
@@ -13,20 +13,21 @@
 #include <complex.h>
 #include <strings.h>
 #include <fcntl.h>
-#define MAXINTER 256
+#define MAXINTER 2048
 #define CHANNEL 3
 #define THRES 2
 #define HDRLEN 18
+#define UPPER 100000
 
 typedef struct
 {
-    uint16_t width;
-    uint16_t height;
-
     double x1;
     double y1;
     double x2;
     double y2;
+
+    uint16_t width;
+    uint16_t height;
 }dim;
 
 void setup(int argc, char **argv, dim *dimensions);
@@ -34,6 +35,8 @@ void setup(int argc, char **argv, dim *dimensions);
 void error(const char *msg) __attribute__((noreturn));
 
 void genset(dim *dimensions, uint8_t *image);
+
+void put_color(uint8_t *image, double pixel_scale, uint16_t n, double complex z, double complex dz);
 
 void fio(dim *dimensions);
 
