d8a5b4ed89c9995f2fdd5b4ccfbec97f05a3c9c2
lscharck <lscharck@gmail.com>
2021-12-25 17:19:26 -0600

    redo macros and add functions

--- include/adds.h
+++ include/adds.h
@@ -1,23 +1,27 @@
 #ifndef DEV_H
 #define DEV_H
 
+#include <complex.h>
+#include <fcntl.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <float.h>
-#include <inttypes.h>
-#include <complex.h>
 #include <strings.h>
-#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
 #define MAXINTER 2048
+#define ESCAPE 2
 #define CHANNEL 3
-#define THRES 2
 #define HDRLEN 18
 #define UPPER 100000
+#define TIME 1000000000.0
+typedef void (*conversion)(char *, void *);
 
 typedef struct
 {
@@ -30,14 +34,20 @@ typedef struct
     uint16_t height;
 }dim;
 
-void setup(int argc, char **argv, dim *dimensions);
+void convd(char *src, void *dst);
+
+void convl(char *src, void *dst);
 
 void error(const char *msg) __attribute__((noreturn));
+
+void error_check(char *src, void *dst, void (*conv) (char *, void *));
+
+void fio(dim *dimensions);
 
 void genset(dim *dimensions, uint8_t *image);
 
 void put_color(uint8_t *image, double pixel_scale, uint16_t n, double complex z, double complex dz);
 
-void fio(dim *dimensions);
+void setup(int argc, char **argv, dim *dimensions);
 
 #endif // DEV_H
