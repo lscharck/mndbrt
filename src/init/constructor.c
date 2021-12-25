9dde1ac7a30a69d62d550bed0c869fc4a5ae6c3b
lscharck <lscharck@gmail.com>
2021-12-25 17:21:48 -0600

    add error controls

--- src/init/constructor.c
+++ src/init/constructor.c
@@ -4,15 +4,85 @@ void setup(int argc, char **argv, dim *dimensions)
 {
 
     if (argc != 7) {
-        error("Not enough inputs!");
+        error("Usage: mndbrt w h x1 y1 x2 y2");
     }
 
-    dimensions->width = (uint16_t)atof(argv[1]);
-    dimensions->height = (uint16_t)atof(argv[2]);
+    error_check(argv[1], &(dimensions->width), convl);
+    error_check(argv[2], &(dimensions->height), convl);
 
-    dimensions->x1 = atof(argv[3]);
-    dimensions->y1 = atof(argv[4]);
-    dimensions->x2 = atof(argv[5]);
-    dimensions->y2 = atof(argv[6]);
+    error_check(argv[3], &(dimensions->x1), convd);
+    error_check(argv[4], &(dimensions->y1), convd);
+    error_check(argv[5], &(dimensions->x2), convd);
+    error_check(argv[6], &(dimensions->y2), convd);
+
+    if (dimensions->x1 >= dimensions->x2 || dimensions->y2 >= dimensions->y1) {
+        error("Improper bounding box");
+    }
+
+}
+
+void error_check(char *src, void *dst, conversion conv)
+{
+
+    conv(src, dst);
+
+}
+
+void convl(char *src, void *dst)
+{
+    char *endptr = NULL;
+    int base = 10;
+    long n;
+    uint16_t *dest = dst;
+
+    n = strtol(src, &endptr, base);
+
+    if (n == 0 && endptr == src) {
+        // check if strtol worked
+        error("Invalid Input(s)");
+    }
+    else if (n == LONG_MIN || n == LONG_MAX) {
+        //  check for underflow or overflow
+        error("Dimension out of spec");
+    }
+    else if (*endptr != '\0') {
+        // check for invalid chars
+        error("Invalid characters");
+    }
+    else if (n <= 0) {
+        // check for negatives
+        error("Negative dimensions");
+    }
+    else if ((uint16_t)n != n) {
+        // check if it fits in 16 bits
+        error("Dimensions out of spec");
+    }
+
+    *dest = n;
+
+}
+
+void convd(char *src, void *dst)
+{
+    char *endptr = NULL;
+    double n;
+    double *dest = dst;
+
+    n = strtod(src, &endptr);
+
+    if (n == 0 && endptr == src) {
+        // check if strtod worked
+        error("Invalid Input(s)");
+    }
+    else if (n == HUGE_VAL || n == HUGE_VALF || n == HUGE_VALL) {
+        //  check for underflow or overflow
+        error("Coords out of spec");
+    }
+    else if (*endptr != '\0') {
+        // check for invalid chars
+        error("Invalid characters");
+    }
+
+    *dest = n;
 
 }
