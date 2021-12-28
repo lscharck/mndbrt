842fdcf42b92c08566a7105a8908936b6a4ed45e
lscharck <lscharck@gmail.com>
2021-12-27 23:52:51 -0600

    call dispatch instead of genset

--- src/fio/img.c
+++ src/fio/img.c
@@ -26,7 +26,8 @@ void fio(dim *dimensions)
 
     strncpy((char*)img, header, HDRLEN);
 
-    genset(dimensions, img + h_size);
+    dispatch(dimensions, img + h_size);
+//    genset(dimensions, img + h_size);
 
     msync(img, size, MS_SYNC);
     munmap(img, size);
