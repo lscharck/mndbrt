17cfbc947c1945b757f241f2f138548a6eb4dcf2
lscharck <lscharck@gmail.com>
2022-01-21 23:57:17 -0600

    add new adt functions

---
+++ include/new.h
@@ -0,0 +1,23 @@
+#ifndef NEW_H
+#define NEW_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "image.h"
+#include "types.h"
+
+void *new(const void *type, ...);
+
+void delete(void *item);
+
+int add(const void *self, ...);
+
+void *clone(const void *self);
+
+int differ(const void *self, const void *b);
+
+size_t sizeOf(const void *self);
+
+#endif // NEW_H
