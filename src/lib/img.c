125682c4e6a24130fce407ee45da1288adf7ddb7
lscharck <lscharck@gmail.com>
2022-01-22 15:01:33 -0600

    clean up

--- src/lib/img.c
+++ src/lib/img.c
@@ -5,15 +5,15 @@ void *image_fio(uint16_t width, uint16_t height)
 
     int f;
     uint8_t *img;
-    uint32_t size, h_size;
-    struct image_info *image = NULL;
-    const char *path = "/Users/lukescharck/Documents/output.ppm";
+    uint32_t size, header_size;
+    struct image_info *image_p = NULL;
     char *header = calloc(1, sizeof(char) * HDRLEN);
+    const char *path = "/Users/lukescharck/Documents/output.ppm";
 
     snprintf(header, HDRLEN, "P6\n%hd %hd 255\n", width, height);
-    h_size = strnlen(header, HDRLEN);
+    header_size = strnlen(header, HDRLEN);
 
-    size = width * height * 3 + h_size;
+    size = width * height * 3 + header_size;
 
     f = open(path, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (f < 0) {printf("Error opening out file\n");}
@@ -22,14 +22,12 @@ void *image_fio(uint16_t width, uint16_t height)
 
     strncpy((char*)img, header, HDRLEN);
 
-    image = calloc(1, sizeof(struct image_info));
-    image->image = img + h_size;
-    image->size = size;
-    image->f = f;
-    image->cnt = 0;
+    image_p = calloc(1, sizeof(struct image_info));
+    image_p->image = img + header_size;
+    image_p->size = size;
+    image_p->f = f;
+    image_p->cnt = 0;
 
-    return image;
-
-    //dispatch(dimensions, img + h_size);
+    return image_p;
 
 }
